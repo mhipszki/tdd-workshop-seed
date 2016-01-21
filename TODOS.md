7434606a857374b41e83a37b6e8ae08ad130808e
Marton Hipszki <mhipszki@gmail.com>
2016-01-21 12:52:41 +0000

    Add a test for two rocks as an input

--- TODOS.md
+++ TODOS.md
@@ -1,7 +1,11 @@
 # Requirements
 
 + the game should be a function
+- The function should return the game's outcome as string
+- The function should have 2 string inputs (shapes)
 
+Examples:
++ input 1 is rock and input 2 is rock: output should be tie
 
 - 2 players
 - 3 shapes : rock, paper, scissors
--- test/rock-paper-scissors.spec.js
+++ test/rock-paper-scissors.spec.js
@@ -1,13 +1,17 @@
 'use strict';
 
 function game() {
-
+	return 'tie';
 }
 
 describe('rock paper scissors game', function () {
 
 	it('should be a function', function () {
 		expect(game).to.be.instanceof(Function);
+	});
+
+	it('should return tie if input 1 is rock and input 2 is rock', function () {
+		expect(game('rock', 'rock')).to.equal('tie');
 	});
 
 });
