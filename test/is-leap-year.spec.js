d913478be5f885e45feedaf35e04bb8ceaa1325f
Marton Hipszki <mhipszki@gmail.com>
2016-01-21 11:10:20 +0000

    isLeapYear returns false when input is divisible by 100 but true when also divisible by 400

--- test/is-leap-year.spec.js
+++ test/is-leap-year.spec.js
@@ -1,6 +1,9 @@
 'use strict';
 
 function isLeapYear(input) {
+	if (input % 100 === 0) {
+		return input % 400 === 0;
+	}
 	return input % 4 === 0;
 }
 
@@ -12,6 +15,14 @@ describe.only('isLeapYear', function () {
 
 	it('should return false when input is not a leap year', function () {
 		expect(isLeapYear(2001)).to.be.false;
+	});
+
+	it('should return false when input is divisible by 100', function () {
+		expect(isLeapYear(1900)).to.be.false;
+	});
+
+	it('should return true when input is divisible by 100 and also by 400', function () {
+		expect(isLeapYear(2000)).to.be.true;
 	});
 
 });
